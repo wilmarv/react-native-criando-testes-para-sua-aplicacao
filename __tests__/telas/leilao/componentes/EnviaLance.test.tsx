3ed4da8c8720e14fa32b8a36db425629b8cd736b
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-10-19 03:08:25 -0300

    04.3 fazendo teste com eventos dos componentes

---
+++ __tests__/telas/leilao/componentes/EnviaLance.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
+import EnviaLance from "../../../../src/telas/Leilao/componentes/EnviaLance";
+import { ENVIADO, NAO_ENVIADO } from "../../../../src/negocio/constantes/estadosLance";
+
+jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
+describe("telas/Leilao/componentes/EnviaLance", () => {
+    it("deve enviar o lance quando o botão for pressionado", async () => {
+        const enviaLance = jest.fn(() => new Promise(resolve => resolve(ENVIADO)));
+
+        const { getByPlaceholderText, getByA11yHint, getByText } = render(
+            <EnviaLance
+                EnviaLance={enviaLance}
+                cor="blue"
+            />
+        );
+        const input = getByPlaceholderText("R$");
+        const botao = getByA11yHint("Enviar lance");
+
+        fireEvent.changeText(input, "10");
+        fireEvent.press(botao);
+
+        expect(enviaLance).toHaveBeenCalledWith("10");
+        // await waitFor(() => {
+        //     expect(getByText(ENVIADO)).toBeTruthy();
+        // });
+
+        // expect((() => getByText(NAO_ENVIADO))).toThrow();
+    });
+});
